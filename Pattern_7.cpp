1043cb4cfc959b8ca8d507ace4b106bea076eae3
Samharvor <theshivthakur@gmail.com>
2026-09-15 20:41:55 +0530

    This is pyramid pattern

---
+++ Pattern_7.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+class Pattern{
+    public: 
+        void pattern7(int n){
+            for(int i=0; i<n; i++){
+                for(int j=0; j<n-i-1; j++){
+                    cout << " ";
+                }
+                for(int j=0; j<2*i+1; j++){
+                    cout << "*";
+                }
+                for(int j=0; j<n-i-1; j++){
+                    cout << " ";
+                }
+                cout << endl;
+            }
+        }
+};
+
+int main(){
+    Pattern p;
+    p.pattern7(9);
+
+}
